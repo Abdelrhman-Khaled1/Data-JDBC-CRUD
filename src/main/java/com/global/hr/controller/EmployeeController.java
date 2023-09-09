291e64dcf17c2da11481ee12536fe1a74bf81cec
Abdelrhman <kabdo17013@gmail.com>
2023-09-09 17:33:26 +0300

    add Service layer

--- src/main/java/com/global/hr/controller/EmployeeController.java
+++ src/main/java/com/global/hr/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.global.hr.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.global.hr.entity.Employee;
 import com.global.hr.repository.EmployeeRepo;
+import com.global.hr.service.EmployeeService;
 
 @RestController
 @RequestMapping("/employee")
@@ -23,52 +25,53 @@ public class EmployeeController {
 	
 	
 	
-//	@Autowired  OR
-	private EmployeeRepo employeeRepo;
-	@Autowired
-	public EmployeeController(EmployeeRepo employeeRepo) {
-		super();
-		this.employeeRepo = employeeRepo;
-	}
+	@Autowired 
+	private EmployeeService employeeService;
+	
 
 //	@RequestMapping(method = RequestMethod.GET , path = "/count")
 	@GetMapping("/count")
 	public long countEmp() {
-		return employeeRepo.count();
+		return employeeService.count();
 	}
 	
 	@GetMapping("/{id}")
-	public Employee findById(@PathVariable Long id) {
-		return employeeRepo.findById(id).get();
+	public ResponseEntity<?> findById(@PathVariable Long id) {
+		return ResponseEntity.ok(employeeService.findById(id));
 	}
 	
 	@GetMapping("")
 	public Iterable<Employee> findAll(){
-		return employeeRepo.findAll();
+		return employeeService.findAll();
 	}
 	
 	@PostMapping("")
 	public Employee addEmp(@RequestBody Employee emp) {
-		return employeeRepo.save(emp);
+		return employeeService.insert(emp);
 	}
 	
 	@PutMapping("")
 	public Employee updateEmp(@RequestBody Employee emp ) {
-		return employeeRepo.save(emp);
+		return employeeService.update(emp);
 	}
 	
 	@GetMapping("/filter/{name}")
 	public List<Employee> filter(@PathVariable String name){
-		return employeeRepo.findByName(name);
+		return employeeService.findByName(name);
 	}
 	
 	@GetMapping("/filter")
 	public List<Employee> filterWithSalary(@RequestParam String name,@RequestParam double salary){
-		return employeeRepo.findByNameAndSalary(name,salary);
+		return employeeService.findByNameAndSalary(name,salary);
 	}
 	
 	@DeleteMapping("/{empId}")
 	public void deleteEmp(@PathVariable("empId") Long id ) {
-		 employeeRepo.deleteById(id);
+		 employeeService.deleteById(id);
+	}
+	
+	@PutMapping("/salary")
+	public int updateSalary(@RequestParam double salary,@RequestParam Long id) {
+		return employeeService.updateSalary(salary,id);
 	}
 }
--- src/main/java/com/global/hr/repository/EmployeeRepo.java
+++ src/main/java/com/global/hr/repository/EmployeeRepo.java
@@ -2,6 +2,8 @@ package com.global.hr.repository;
 
 import java.util.List;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,8 @@ import com.global.hr.entity.Employee;
 public interface EmployeeRepo extends CrudRepository<Employee, Long>{
 	public List<Employee> findByName(String name);
 	public List<Employee> findByNameAndSalary(String name, double salary); //Wow
+	
+	@Modifying
+	@Query(value = "update employee set salary = :salary where id = :id ")
+	int updateSalary(Double salary , Long id);
 }
